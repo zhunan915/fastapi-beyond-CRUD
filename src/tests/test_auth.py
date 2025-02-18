ab51298b95a6eec0dd186bdc0cfa3e7175130a47
zhunan915 <zhunan@bu.edu>
2025-02-18 11:27:43 +0800

    update testing incorrect tests data

--- src/tests/test_auth.py
+++ src/tests/test_auth.py
@@ -19,7 +19,8 @@ def test_user_creation(fake_session, fake_user_service, test_client):
     user_data = UserCreateModel(**signup_data)
 
     assert fake_user_service.user_exists_called_once()
-    assert fake_user_service.user_exists_called_once_with(signup_data['email'],fake_session)
+    # assert fake_user_service.user_exists_called_once_with(signup_data['email'],fake_session)
+    assert fake_user_service.user_exists_called_once_with("wrongemail@gmail.com", fake_session)
     assert fake_user_service.create_user_called_once()
     assert fake_user_service.create_user_called_once_with(user_data,fake_session)
 
